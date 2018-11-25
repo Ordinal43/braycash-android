63309a4232e9764331fa736670939c81d6fdfa72
Bryan Ramaputra <bryan.ramaputra@ti.ukdw.ac.id>
2018-11-25 18:24:55 +0700

    Added payment functionality

--- app/src/main/java/id/ac/ukdw/braycash/Home/PaymentActivity.java
+++ app/src/main/java/id/ac/ukdw/braycash/Home/PaymentActivity.java
@@ -20,14 +20,17 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
-import id.ac.ukdw.braycash.Login.VerifyRegisterActivity;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import id.ac.ukdw.braycash.Database.Transaction;
 import id.ac.ukdw.braycash.R;
 
-public class ScanPaymentActivity extends AppCompatActivity {
+public class PaymentActivity extends AppCompatActivity {
 
-    private static final String TAG = "ScanPaymentActivity";
+    private static final String TAG = "PaymentActivity";
 
     private Context mContext;
     private Button btnPayment;
@@ -36,8 +39,8 @@ public class ScanPaymentActivity extends AppCompatActivity {
 
     private EditText transferAmountText;
 
-    private String phoneNumberText;
-    private Long saldo;
+    private String recipientId, recipientPhone;
+    private Long mySaldo, recipientSaldo;
 
 
     // firebase stuff
@@ -53,7 +56,7 @@ public class ScanPaymentActivity extends AppCompatActivity {
 
         initWidgets();
 
-        getRecipientName();
+        getRecipient();
         getMyBalance();
 
         backBtn.setOnClickListener(new View.OnClickListener() {
@@ -68,21 +71,83 @@ public class ScanPaymentActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 //check if balance is enough
-                Long transferAmount = Long.valueOf(transferAmountText.getText().toString());
-                if(saldo < transferAmount) {
+                String amountString = transferAmountText.getText().toString();
+
+                if(amountString.isEmpty()) {
+                    Toast.makeText(
+                            mContext,
+                            "You must input a number",
+                            Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                Long transferAmount = Long.valueOf(amountString);
+                if(transferAmount < 5000) {
+                    Toast.makeText(
+                            mContext,
+                            "Minimum transaction is Rp 5000",
+                            Toast.LENGTH_SHORT).show();
+                } else if(mySaldo < transferAmount) {
                     Toast.makeText(
                             mContext,
                             "Not enough balance!",
                             Toast.LENGTH_SHORT).show();
                 } else {
 
-                    finish();
-                    Intent intent = new Intent(mContext, TransactionSuccessActivity.class);
-                    startActivity(intent);
+                    if(makePayment(transferAmount)) {
+                        finish();
+                        Intent intent = new Intent(mContext, TransactionSuccessActivity.class);
+                        startActivity(intent);
+                    }
+
                 }
 
             }
         });
+    }
+
+    private boolean makePayment(Long transferAmount) {
+        //set dateformat
+        DateFormat df = new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss");
+        String dateString = df.format(Calendar.getInstance().getTime());
+
+        // find reference for current account
+        String uid = mAuth.getUid();
+        String myPhone = mAuth.getCurrentUser().getPhoneNumber();
+
+        /**
+         * get the reference to the current user and recipient
+         */
+        DatabaseReference paymentRef = mFirebaseDatabase
+                .getReference("users/" + uid);
+        DatabaseReference receivedRef = mFirebaseDatabase
+                .getReference("users/" + recipientId);
+
+        /**
+         * set the new balance for both users
+         */
+        paymentRef.child("saldo").setValue(mySaldo - transferAmount);
+        receivedRef.child("saldo").setValue(recipientSaldo + transferAmount);
+
+        /**
+         * add new transaction record for payment and receiving
+         */
+        String idPayment = paymentRef.push().getKey();
+        paymentRef.child("payments").child(idPayment).setValue(new Transaction(
+                transferAmount,
+                recipientPhone,
+                dateString
+        ));
+
+        String idReceived = receivedRef.push().getKey();
+        receivedRef.child("received").child(idReceived).setValue(new Transaction(
+                transferAmount,
+                myPhone,
+                dateString
+        ));
+
+
+        return true;
     }
 
     private void initWidgets() {
@@ -92,7 +157,7 @@ public class ScanPaymentActivity extends AppCompatActivity {
         myRef = mFirebaseDatabase.getReference();
         userRef = mFirebaseDatabase.getReference("users");
 
-        mContext = ScanPaymentActivity.this;
+        mContext = PaymentActivity.this;
         btnPayment = (Button) findViewById(R.id.btnPayment);
         backBtn = (ImageView) findViewById(R.id.backArrow);
 
@@ -101,81 +166,53 @@ public class ScanPaymentActivity extends AppCompatActivity {
         currentSaldo = (TextView) findViewById(R.id.currentSaldo);
         transferAmountText = (EditText) findViewById(R.id.transferAmount);
 
+        recipientPhone = getIntent().getStringExtra("RECIPIENT_PHONE");
 
-        phoneNumberText = getIntent().getStringExtra("RECIPIENT_PHONE");
-
-        recipientNumber.setText(phoneNumberText);
+        recipientNumber.setText(recipientPhone);
 
     }
 
-    private void getRecipientName(){
-
-        String recipientPhone = recipientNumber.getText().toString();
+    private void getRecipient(){
 
         userRef.orderByChild("phone")
                 .equalTo(recipientPhone)
                 .addChildEventListener(new ChildEventListener() {
                     @Override
                     public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                        recipientId = dataSnapshot.getKey();
+                        recipientPhone = dataSnapshot.child("phone").getValue().toString();
+                        recipientSaldo = Long.valueOf(dataSnapshot.child("saldo").getValue().toString());
+
                         String name = dataSnapshot.child("name").getValue().toString();
                         recipientName.setText(name);
                     }
 
                     @Override
                     public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                        recipientId = dataSnapshot.getKey();
+                        recipientPhone = dataSnapshot.child("phone").getValue().toString();
+                        recipientSaldo = Long.valueOf(dataSnapshot.child("saldo").getValue().toString());
+
                         String name = dataSnapshot.child("name").getValue().toString();
                         recipientName.setText(name);
                     }
 
                     @Override
                     public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
-
-                    }
-
-                    @Override
-                    public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
                         Toast.makeText(
                                 mContext,
-                                "There was an error. Check your connection and try again",
+                                "Data not found!",
                                 Toast.LENGTH_SHORT).show();
                         finish();
                     }
-        });
-
-    }
-
-    private void getMyBalance(){
-
-        String myPhone = mAuth.getCurrentUser().getPhoneNumber();
-
-        userRef.orderByChild("phone")
-                .equalTo(myPhone)
-                .addChildEventListener(new ChildEventListener() {
-                    @Override
-                    public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-                        saldo = Long.valueOf(dataSnapshot.child("saldo").getValue().toString());
-                        currentSaldo.setText("Rp " + saldo);
-                    }
-
-                    @Override
-                    public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-                        saldo = Long.valueOf(dataSnapshot.child("saldo").getValue().toString());
-                        currentSaldo.setText("Rp " + saldo);
-                    }
-
-                    @Override
-                    public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
-
-                    }
 
                     @Override
                     public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-
+                        Toast.makeText(
+                                mContext,
+                                "Data not found!",
+                                Toast.LENGTH_SHORT).show();
+                        finish();
                     }
 
                     @Override
@@ -188,6 +225,54 @@ public class ScanPaymentActivity extends AppCompatActivity {
                     }
                 });
 
+    }
+
+    private void getMyBalance(){
+
+        String myPhone = mAuth.getCurrentUser().getPhoneNumber();
+
+        userRef.orderByChild("phone")
+                .equalTo(myPhone)
+                .addChildEventListener(new ChildEventListener() {
+                    @Override
+                    public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                        mySaldo = Long.valueOf(dataSnapshot.child("saldo").getValue().toString());
+                        currentSaldo.setText("Rp " + mySaldo);
+                    }
+
+                    @Override
+                    public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                        mySaldo = Long.valueOf(dataSnapshot.child("saldo").getValue().toString());
+                        currentSaldo.setText("Rp " + mySaldo);
+                    }
+
+                    @Override
+                    public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+                        Toast.makeText(
+                                mContext,
+                                "Data not found!",
+                                Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+
+                    @Override
+                    public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                        Toast.makeText(
+                                mContext,
+                                "Data not found!",
+                                Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        Toast.makeText(
+                                mContext,
+                                "There was an error. Check your connection and try again",
+                                Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                });
     }
 
 }

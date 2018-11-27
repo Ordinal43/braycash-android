c94590deec5ee1fa8bdc1f277331a6fa873293f0
Bryan Ramaputra <bryan.ramaputra@ti.ukdw.ac.id>
2018-11-28 01:24:54 +0700

    reworked sign out menu

--- app/src/main/java/id/ac/ukdw/braycash/Profile/SignOutFragment.java
+++ app/src/main/java/id/ac/ukdw/braycash/Profile/SignOutFragment.java
@@ -8,26 +8,59 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
+import id.ac.ukdw.braycash.Login.LoginActivity;
 import id.ac.ukdw.braycash.R;
-import id.ac.ukdw.braycash.Utils.SplashActivity;
 
 public class SignOutFragment extends Fragment {
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+    private ProgressBar mProgressBar;
+    private ImageView backArrow;
+    private TextView tvSignout;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_sign_out, container, false);
-        FirebaseAuth.getInstance().signOut();
 
-        Intent intent = new Intent(this.getContext(), SplashActivity.class);
+        mAuth = FirebaseAuth.getInstance();
 
-        // Erase all previous intents
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
 
-        startActivity(intent);
+        tvSignout = (TextView) view.findViewById(R.id.tvConfirmSignout);
+        mProgressBar = (ProgressBar) view.findViewById(R.id.progressBar);
+        backArrow = (ImageView) view.findViewById(R.id.backArrow);
+
+        mProgressBar.setVisibility(View.GONE);
+        Button btnConfirmSignout = (Button) view.findViewById(R.id.btnConfirmSignout);
+
+        btnConfirmSignout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mProgressBar.setVisibility(View.VISIBLE);
+                mAuth.signOut();
+
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        });
+
+        backArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().finish();
+            }
+        });
         return view;
     }
 }

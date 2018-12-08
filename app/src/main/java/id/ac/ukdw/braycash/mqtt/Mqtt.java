d5ea97c6d8141a5dbe9a3e2de899e64620bdcb42
samuel ricky <komputerindo21@gmail.com>
2018-12-08 19:47:47 +0700

    MQTT
    MQTT

---
+++ app/src/main/java/id/ac/ukdw/braycash/mqtt/Mqtt.java
@@ -0,0 +1,93 @@
+package id.ac.ukdw.braycash.mqtt;
+
+import android.content.Context;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+import java.io.UnsupportedEncodingException;
+
+public class Mqtt {
+    private String username;
+    private String password;
+    MqttAndroidClient client;
+
+    public Mqtt(String username, String password)
+    {
+        this.username = username;
+        this.password = password;
+    }
+    public void konek(Context ctx) throws MqttException {
+        String clientId = MqttClient.generateClientId();
+        client = new MqttAndroidClient(ctx, "tcp://broker.shiftr.io:1883", clientId);
+
+        try {
+            IMqttToken token = client.connect();
+            token.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    // We are connected
+
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    // Something went wrong e.g. connection timeout or firewall problems
+
+
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setMqttVersion(MqttConnectOptions.MQTT_VERSION_3_1);
+
+        options.setUserName(this.username);
+        options.setPassword(this.password.toCharArray());
+
+        IMqttToken token = client.connect(options);
+    }
+    public void publish(String topic, String payload){
+
+        byte[] encodedPayload = new byte[0];
+        try {
+            encodedPayload = payload.getBytes("UTF-8");
+            MqttMessage message = new MqttMessage(encodedPayload);
+            this.client.publish(topic, message);
+        } catch (UnsupportedEncodingException | MqttException e) {
+            e.printStackTrace();
+        }
+    }
+    public void subscribe(String topic)
+    {
+
+        int qos = 1;
+        try {
+            IMqttToken subToken = client.subscribe(topic, qos);
+            subToken.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    // The message was published
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken,
+                                      Throwable exception) {
+                    // The subscription could not be performed, maybe the user was not
+                    // authorized to subscribe on the specified topic e.g. using wildcards
+
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
